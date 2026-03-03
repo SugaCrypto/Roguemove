791f7094036f1ffafaf40f3687300784ae745a33
SugaCrypto <sugacrypto5613@gmail.com>
2026-03-04 01:03:10 +0900

    fix: update test to match new UI and cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders ENTER DUNGEON button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/ENTER DUNGEON/i);
+  expect(buttonElement).toBeInTheDocument();
 });
